b812a11dfe64943e7c9370aeac7e29d72633581d
Nino Papava <99906660+NinoPapava@users.noreply.github.com>
2022-05-21 18:55:12 +0400

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=id18837538_test', 'id18837538_test123', 'L0Uqh!}IJUhOYZG-');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  return $pdo;
+?>
